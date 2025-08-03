f9e5e9f87a9bb463776adbae16f169deaef8f3f3
hassamaziz <hassamaziz001@gmail.com>
2025-08-03 22:28:40 +0500

    feat: implement MongoDB connection and product model

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
+app.post("/products", (req, res) => {
+    
 });
 
 app.listen(5000, () => {
+    connectDB();
   console.log("Server is running on port 5000");
 });
 
