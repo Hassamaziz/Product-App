d96c3be6c40baa905e2aa453396c6929073b36d3
hassamaziz <hassamaziz001@gmail.com>
2025-08-07 20:58:53 +0500

    .

--- backend/server.js
+++ backend/server.js
@@ -42,9 +42,8 @@ app.delete("/api/products/:id", async (req, res) => {
 });
 
 
-
 app.listen(5000, () => {
-    connectDB();
+  connectDB();
   console.log("Server is running on port 5000");
 });
 
