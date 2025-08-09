75da203a06ea8a7746e6263de1386e482b963c68
hassamaziz <hassamaziz001@gmail.com>
2025-08-09 21:29:12 +0500

    Remove unused imports from product routes for cleaner code

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,17 +1,11 @@
 import express from "express";
-import Product from "../models/product.model.js";
-import mongoose from "mongoose";
 import { createProduct, deleteProduct, getProduct, updateProduct } from "../controllers/product.controller.js";
 
 const router = express.Router();
 
 router.get("/", getProduct);
 
-
-
 router.post("/", createProduct);
-
-
 
 router.delete("/:id", deleteProduct);
 
