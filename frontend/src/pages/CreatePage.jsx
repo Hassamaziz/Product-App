1bbeb0c1d6ae9419dbca54ca38fea7f4c64f5ab0
hassamaziz <hassamaziz001@gmail.com>
2025-08-11 19:26:32 +0500

    feat: implement CreatePage component with product input fields and layout

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import { Box, Container, Heading, Input, useColorModeValue, VStack } from '@chakra-ui/react';
+import React, { useState } from 'react'
 
 const CreatePage = () => {
+  const [newProduct, setNewProduct] = useState({
+    name: '',
+    price: 0,
+    image: ''
+  });
+
   return (
-    <div>CreatePage</div>
+    <Container maxW={"container.sm"}>
+      <VStack spacing={8}>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>Create New Product</Heading>
+
+        <Box
+        w={"full"} bg={useColorModeValue('white', 'gray.800')} 
+        p={6} rounded={"lg"} shadow={"md"}
+        >
+          <VStack spacing={4}>
+            <Input
+              placeholder="Product Name"
+              value={newProduct.name}
+              onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+            />
+            <Input
+              placeholder="Product Price"
+              type="number"
+              value={newProduct.price}
+              onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
+            />
+            <Input
+              placeholder="Product Image URL"
+              value={newProduct.image}
+              onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })}
+            />
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
   )
 }
 
