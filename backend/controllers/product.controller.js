16358bddcb6dca2c3dd43b01e006dfcb022708a9
hassamaziz <hassamaziz001@gmail.com>
2025-08-21 23:57:37 +0500

    .

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -18,7 +18,6 @@ export const createProduct = async (req, res) => {
 	if (!product.name || !product.price || !product.image) {
 		return res.status(400).json({ success: false, message: "Please provide all fields" });
 	}
-
 	const newProduct = new Product(product);
 
 	try {
